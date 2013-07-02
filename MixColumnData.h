f20e713d65b0f9867d90eb9490e0465bafa6f06c
Ravi Malik <ravimalik2364@gmail.com>
2013-07-02 18:43:43 +0530

    Added the GNU GPL v2 License to the Project.

--- MixColumnData.h
+++ MixColumnData.h
@@ -1,3 +1,24 @@
+/*==========================================================================================
+||	
+||	Copyright (C) 2013  Ravi Malik
+||	
+||	This program is free software; you can redistribute it and/or
+||	modify it under the terms of the GNU General Public License
+||	as published by the Free Software Foundation; either version 2
+||	of the License, or (at your option) any later version.
+||	
+||	This program is distributed in the hope that it will be useful,
+||	but WITHOUT ANY WARRANTY; without even the implied warranty of
+||	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+||	GNU General Public License for more details.
+||	
+||	You should have received a copy of the GNU General Public License
+||	along with this program; if not, write to the Free Software
+||	Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+||	
+||
+*==========================================================================================*/
+
 unsigned char mix_col_2[]={
 0x00,0x02,0x04,0x06,0x08,0x0a,0x0c,0x0e,0x10,0x12,0x14,0x16,0x18,0x1a,0x1c,0x1e,
 0x20,0x22,0x24,0x26,0x28,0x2a,0x2c,0x2e,0x30,0x32,0x34,0x36,0x38,0x3a,0x3c,0x3e,
