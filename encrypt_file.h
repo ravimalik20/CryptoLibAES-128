7c7587cc3e1a082ba37c8c1341a06936e15bbc16
Ravi Malik <ravimalik2364@gmail.com>
2013-07-07 18:33:56 +0530

    Added the decrypting logics with test program.

--- encrypt_file.h
+++ encrypt_file.h
@@ -58,3 +58,66 @@ void encrypt_file(FILE *fp_i, struct block key, FILE *fp_o)
 		}
 	}
 }
+
+int decrypt_file(FILE *fp_i, struct block key, FILE *fp_o)
+{	//Takes the FILE pointer of the file to decrypt, block object containing the key and the file pointer of the file to place decrypted data. It does not close the file pointers passed as arguments and it is the responsibility of the programmer to close it while using it in a wrapper function.
+	struct block data;
+	struct block round_key[11];
+	generate_round_keys(key,round_key);	
+	unsigned long size=file_size(fp_i);
+	int a,b;
+	a=size/16;	// Number of 4*4 blocks fetchable from file.
+	b=size%16;	// Number of bytes left to be fetched into the last block of the file.
+	int i;		// Counter of number of complete 4*4 blocks fetchable from file.
+	int j,k;
+	for(i=0;i<a-1;i++)
+	{	for(j=0;j<4;j++)
+		{	for(k=0;k<4;k++)
+			{	fread(&data.b[j][k],sizeof(data.b[j][k]),1,fp_i);
+			}
+		}
+		data=decrypt(data,round_key);
+		for(j=0;j<4;j++)
+		{	for(k=0;k<4;k++)
+			{	fwrite(&data.b[j][k],sizeof(data.b[j][k]),1,fp_o);
+			}
+		}
+	}
+	for(j=0;j<4;j++)
+	{	for(k=0;k<4;k++)
+		{	fread(&data.b[j][k],sizeof(data.b[j][k]),1,fp_i);
+		}
+	}
+	data=decrypt(data,round_key);
+	char padd_size;
+	padd_size=data.b[3][3];
+	int c=(int)padd_size;
+	printf("Padd_size:%d",c);
+	j=3;
+	k=3;
+	while(c!=0)
+	{	if(k<0)
+		{	k=3;
+			j--;
+		}
+		if(data.b[j][k]!=c)
+		{	return -1;
+		}
+		k--;	
+		c--;	
+	}
+	c=16-((int)padd_size);
+	printf("Padd_size:%d",c);
+	j=0;
+	k=0;
+	while(c!=0)
+	{	if(k==4)
+		{	k=0;
+			j++;
+		}
+		fwrite(&data.b[j][k],sizeof(data.b[j][k]),1,fp_o);
+		k++;
+		c--;
+	}
+	return 0;
+}
---
+++ test_decrypt_file.c
@@ -0,0 +1,13 @@
+#include"encrypt_file.h"
+
+void main(int argc, char* argv[])
+{	struct block key;
+	FILE *fp1,*fp2;
+	fp1=fopen(argv[1],"rb");
+	fp2=fopen(argv[3],"wb");
+	// Converting the key provided as string into key object.
+	key=prepare_key(argv[2]);
+	decrypt_file(fp1,key,fp2);
+	fclose(fp1);
+	fclose(fp2);
+}
