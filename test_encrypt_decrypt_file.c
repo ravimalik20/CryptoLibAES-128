b09e4eaa59ca4e76a039792bbd57f376bc07258d
Ravi Malik <ravimalik2364@gmail.com>
2017-09-17 00:51:35 -0400

    Code Refactoring.
    Refactored the code with a better interface and better
    organization of files. It now follows the standard interface
    based coding standard followed by other C libraries, i.e.,
    interface in aes.h and implementation in aes.c. Earlier all the
    code was in aes.h. It was stupidly done by me as I didn't
    understand the concept of C interface.

--- test_encrypt_decrypt_file.c
+++ test_encrypt_decrypt_file.c
@@ -19,50 +19,38 @@
 ||
 *==========================================================================================*/
 
-#include<stdio.h>
-#include"MixColumnData.h"
+#include"aes.h"
 
-struct block
-{	unsigned char b[4][4];
-};
+void encrypt()
+{
+	FILE *fp_input, *fp_output;
+	fp_input = fopen("README", "rb");
+	fp_output = fopen("readme.enc", "wb");
 
-struct block mix_column(struct block data,int matrix[4][4])
-{	struct block temp;
-	int i,j,k;
-	int x,y;
-	unsigned char r;
-	for(i=0;i<4;i++)
-	{	for(j=0;j<4;j++)
-		{	temp.b[i][j]=0x00;
-			for(k=0;k<4;k++)
-			{	x=matrix[i][k];
-				y=(int)data.b[k][j];
-				switch(x)
-				{	case 1:
-						r=y;
-						break;
-					case 2:
-						r=mix_col_2[y];
-						break;
-					case 3:
-						r=mix_col_3[y];
-						break;
-					case 9:
-						r=mix_col_9[y];
-						break;
-					case 11:
-						r=mix_col_11[y];
-						break;
-					case 13:
-						r=mix_col_13[y];
-						break;
-					case 14:
-						r=mix_col_14[y];
-						break;					
-				}	
-				temp.b[i][j]=temp.b[i][j]^(r);
-			}
-		}
-	}
-	return temp;
+	char *key = "mynameisslimshad";
+
+	aes_encrypt_file(fp_input, fp_output, key);
+
+	fclose(fp_input);
+	fclose(fp_output);
+}
+
+void decrypt()
+{
+	FILE *fp_input, *fp_output;
+	fp_input = fopen("readme.enc", "rb");
+	fp_output = fopen("readme.dec", "wb");
+
+	char *key = "mynameisslimshad";
+
+	aes_decrypt_file(fp_input, fp_output, key);
+
+	fclose(fp_input);
+	fclose(fp_output);
+}
+
+void main(int argc, char* argv[])
+{	
+	encrypt();
+	decrypt();
 }
